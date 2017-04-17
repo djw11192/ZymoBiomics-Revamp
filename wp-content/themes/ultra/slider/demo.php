4c488d704d35afcc6504b9c16957f52d0503368a
David Weber <dweber@zymoresearch.com>
2017-04-17 10:53:11 -0700

    added placeholderr images for slider and set up child theme

--- wp-content/themes/ultra/slider/demo.php
+++ wp-content/themes/ultra/slider/demo.php
@@ -2,15 +2,16 @@
 	<ul class="slides">
 		<li>
 			<div class="content">
-				<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/demo-overlay-01.png" width="1200" height="480" alt="<?php esc_attr_e('Demo Slide', 'ultra') ?>" />
+				<!--<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/zymobiomics-launch.jpg" width="1200" height="480" alt="<?php esc_attr_e('Demo Slide', 'ultra') ?>" />-->
 			</div>
-			<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/demo-slide-01.jpg" class="ms-default-image" width="1200" height="480" alt="<?php esc_attr_e('Demo Slide', 'ultra') ?>" />
+<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/zymobiomics-launch.jpg" class="ms-default-image" width="1200" height="480" alt="<?php esc_attr_e('Demo Slide', 'ultra') ?>" />
+			
 		</li>
 		<li>
 			<div class="content">
-				<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/demo-overlay-02.png" width="1200" height="480" alt="<?php esc_attr_e('Demo Slide', 'ultra') ?>" />
+				<!--<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/demo-overlay-02.png" width="1200" height="480" alt="<?php esc_attr_e('Demo Slide', 'ultra') ?>" />-->
 			</div>		
-			<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/demo-slide-02.jpg" class="ms-default-image" width="1200" height="480" alt="<?php esc_attr_e('Demo Slide', 'ultra') ?>" />
+			<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/lifestyle-banner.jpg" class="ms-default-image" width="1200" height="480" alt="<?php esc_attr_e('Demo Slide', 'ultra') ?>" />
 		</li>
 	</ul>
 </div><!-- .flexslider #metaslider-demo -->
