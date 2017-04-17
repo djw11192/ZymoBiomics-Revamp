47aa329a57fe9f993dda633b3cb3c1fed90f5597
David Weber <dweber@zymoresearch.com>
2017-04-17 09:56:33 -0700

    videos added and widgets installed

--- wp-content/themes/ultra/sidebar.php
+++ wp-content/themes/ultra/sidebar.php
@@ -7,7 +7,7 @@
  * @package ultra
  * @since ultra 0.9
  * @license GPL 2.0
- */
+ 
 
 	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 		return;
@@ -19,3 +19,5 @@
 <div id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->
+
+*/
