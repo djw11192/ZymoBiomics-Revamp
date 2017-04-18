e3fdd7d959c355de8cddcebdf1809eec1c33399c
David Weber <dweber@zymoresearch.com>
2017-04-18 10:22:59 -0700

    added styling to home page

--- wp-content/themes/ultra/slider/demo.php
+++ wp-content/themes/ultra/slider/demo.php
@@ -11,7 +11,7 @@
 			<div class="content">
 				<!--<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/demo-overlay-02.png" width="1200" height="480" alt="<?php esc_attr_e('Demo Slide', 'ultra') ?>" />-->
 			</div>		
-			<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/lifestyle-banner.jpg" class="ms-default-image" width="1200" height="480" alt="<?php esc_attr_e('Demo Slide', 'ultra') ?>" />
+			<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/lifestyle-banner.jpg" class="ms-default-image" width="1154" height="512" alt="<?php esc_attr_e('Demo Slide', 'ultra') ?>" />
 		</li>
 	</ul>
 </div><!-- .flexslider #metaslider-demo -->
