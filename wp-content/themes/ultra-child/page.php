929be70faff65af7992fe223880cc5a6e959df5b
David Weber <dweber@zymoresearch.com>
2017-04-18 15:54:32 -0700

    styled page and added table. Also changed table on community standards

--- wp-content/themes/ultra-child/page.php
+++ wp-content/themes/ultra-child/page.php
@@ -63,11 +63,7 @@ get_header(); ?>
 				(function(){
 					var tableSpace = document.querySelector('.orderTable');
 					if(url.includes("microbial-community-standards")){
-						prodRow = '<tr><td>ZymoBIOMICS Microbial Community Standard</td><td>D6300</td><td>10 Preps</td><td>$250.00</td><td class="inputCell"><input style="padding:0; " type="text" name="qty[1651]" maxlength="12" placeholder="0" title="Qty" class="input-text qty" id="qtyinput" onchange="save_id(1651, this.value)" prodid="1651"></td></tr>' +
-						
-						'<tr><td>ZymoBIOMICS Microbial Community DNA Standard</td><td>D6305</td><td>200 ng</td><td>$104.00</td><td class="inputCell"><input style="padding:0; " type="text" name="qty[1653]" maxlength="12" placeholder="0" title="Qty" class="input-text qty" id="qtyinput" onchange="save_id(1653, this.value)" prodid="1653"></td></tr>' +
-						
-						'<tr><td>ZymoBIOMICS Microbial Community DNA Standard</td><td>D6306</td><td>2000 ng</td><td>$208.00</td><td class="inputCell"><input style="padding:0; " type="text" name="qty[1654]" maxlength="12" placeholder="0" title="Qty" class="input-text qty" id="qtyinput" onchange="save_id(1654, this.value)" prodid="1654"></td></tr>'
+						prodRow = '<tr><td>ZymoBIOMICS Microbial Community Standard</td><td>D6300</td><td>10 Preps</td><td>$250.00</td><td class="inputCell"><input style="padding:0; " type="text" name="qty[1651]" maxlength="12" placeholder="0" title="Qty" class="input-text qty" id="qtyinput" onchange="save_id(1651, this.value)" prodid="1651"></td></tr>'
 						
 					} else if(url.includes("dnarna-shield")){
 						prodRow = '<tr><td>DNA/RNA Shield</td><td>R1100-50</td><td>50 ml</td><td>$68.00</td><td class="inputCell"><input style="padding:0; " type="text" name="qty[1133]" maxlength="12" placeholder="0" title="Qty" class="input-text qty" id="qtyinput" onchange="save_id(1133, this.value)" prodid="1133"></td></tr>' + 
@@ -86,16 +82,10 @@ get_header(); ?>
 						'<tr><td>ZymoBIOMICS DNA Miniprep Kit (Lysis Matrix Not Included)</td><td>D4304</td><td>50 Preps</td><td>$202.00</td><td class="inputCell"><input style="padding:0; " type="text" name="qty[1697]" maxlength="12" placeholder="0" title="Qty" class="input-text qty" id="qtyinput" onchange="save_id(1697, this.value)" prodid="1697"></td></tr>' 
 						
 						
-					} else if(url === "midi-maxi-giga"){
-						prodRow = '<tr><td>ZymoPURE™ Plasmid Midiprep Kit</td><td>D4200</td><td>25 Preps</td><td>$255.00</td><td class="inputCell"><input style="padding:0; " type="text" name="qty[1487]" maxlength="12" placeholder="0" title="Qty" class="input-text qty" id="qtyinput" onchange="save_id(1487, this.value)" prodid="1487"></td></tr>' +
+					} else if(url.includes("dna-standards")){
+						prodRow =  	'<tr><td>ZymoBIOMICS Microbial Community DNA Standard</td><td>D6305</td><td>200 ng</td><td>$104.00</td><td class="inputCell"><input style="padding:0; " type="text" name="qty[1653]" maxlength="12" placeholder="0" title="Qty" class="input-text qty" id="qtyinput" onchange="save_id(1653, this.value)" prodid="1653"></td></tr>' +
 						
-						'<tr><td>ZymoPURE™ Plasmid Midiprep Kit</td><td>D4201</td><td>50 Preps</td><td>$458.00</td><td class="inputCell"><input style="padding:0; " type="text" name="qty[1488]" maxlength="12" placeholder="0" title="Qty" class="input-text qty" id="qtyinput" onchange="save_id(1488, this.value)" prodid="1488"></td></tr>' +
-						
-						'<tr><td>ZymoPURE™ Plasmid Maxiprep Kit</td><td>D4202</td><td>10 Preps</td><td>$206.00</td><td class="inputCell"><input style="padding:0; " type="text" name="qty[1484]" maxlength="12" placeholder="0" title="Qty" class="input-text qty" id="qtyinput" onchange="save_id(1484, this.value)" prodid="1484"></td></tr>' +
-						
-						'<tr><td>ZymoPURE™ Plasmid Maxiprep Kit</td><td>D4203</td><td>20 Preps</td><td>$398.00</td><td class="inputCell"><input style="padding:0; " type="text" name="qty[1485]" maxlength="12" placeholder="0" title="Qty" class="input-text qty" id="qtyinput" onchange="save_id(1485, this.value)" prodid="1485"></td></tr>' +
-						
-						'<tr><td>ZymoPURE™ Plasmid Gigaprep Kit</td><td>D4204</td><td>5 Preps</td><td>$462.00</td><td class="inputCell"><input style="padding:0; " type="text" name="qty[1490]" maxlength="12" placeholder="0" title="Qty" class="input-text qty" id="qtyinput" onchange="save_id(1490, this.value)" prodid="1490"></td></tr>'
+						'<tr><td>ZymoBIOMICS Microbial Community DNA Standard</td><td>D6306</td><td>2000 ng</td><td>$208.00</td><td class="inputCell"><input style="padding:0; " type="text" name="qty[1654]" maxlength="12" placeholder="0" title="Qty" class="input-text qty" id="qtyinput" onchange="save_id(1654, this.value)" prodid="1654"></td></tr>'
 						
 					}
 					tableSpace.innerHTML = '<form action="/nicktest" method="post"><table border="1" class="order_table"><thead><tr class="order_table_head"><th>Product</th><th>Cat. No.</th><th>Size</th><th>Price</th><th>Qty</th></tr></thead><tbody>' + prodRow + '</tbody></table><input class="btn" style="float: right; padding: 10px 10px; margin-top: 18px; font-weight: bold;background-color: #1B9644;font-size: 22px;color: white;border-radius: 5px;border: none;cursor: pointer;" onclick="submitProducts();return false;" type="button" value="Add To Cart"></form>'
